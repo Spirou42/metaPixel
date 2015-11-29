7b9cd55260e544987c2d9b9f8df42b3b959e5849
Carsten Müller <carstenm@mac.com>
2015-11-29 18:39:15 +0100

    argl

--- Software/metaTFT/metaTFT.ino
+++ Software/metaTFT/metaTFT.ino
@@ -20,28 +20,10 @@
 #include "GraphicTests.h"
 #include "UIHelpers.h"
 #include "font_Montserrat_Regular.h"
+#include "metaTFT.h"
 
 
 
-
-/** TFT Configuration **/
-#define TFT_RST   2
-#define TFT_DC    9
-#define TFT_CS    10
-#define TFT_MISO  12
-#define TFT_MOSI  11
-#define TFT_SCK   13
-#define TFT_LED   6
-//A14 || 6
-
-/** FastLed**/
-#define COLOR_ORDER       GRB
-#define CHIPSET           WS2812
-#define COLOR_CORRECTION  0xffeeff
-#define BRIGHTNESS        160
-
-#define LED_PIN                   4
-#define NUM_LEDS 60
 CRGB  leds[NUM_LEDS];
 
 
